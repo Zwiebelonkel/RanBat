1b81331f6d792dba8b6f219199b883b6a34641f7
lucamuller2004 <lucamuller2004@gmail.com>
2025-10-17 18:24:54 +0000

    fix

--- battler/src/app/services/theme.service.ts
+++ battler/src/app/services/theme.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -6,22 +7,28 @@ import { Injectable } from '@angular/core';
 export class ThemeService {
   private isDarkMode = false;
 
-  constructor() {
-    this.isDarkMode = localStorage.getItem('theme') === 'dark';
-    this.updateTheme();
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDarkMode = localStorage.getItem('theme') === 'dark';
+      this.updateTheme();
+    }
   }
 
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
-    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
-    this.updateTheme();
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+      this.updateTheme();
+    }
   }
 
   private updateTheme() {
-    if (this.isDarkMode) {
-      document.body.classList.add('dark-theme');
-    } else {
-      document.body.classList.remove('dark-theme');
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.isDarkMode) {
+        document.body.classList.add('dark-theme');
+      } else {
+        document.body.classList.remove('dark-theme');
+      }
     }
   }
 }
