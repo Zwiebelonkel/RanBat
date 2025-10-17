a9cd26abcbb290d3e9b74e1f7192c7905e12e8a0
Zwiebelonkel <lucamuller2004@gmail.com>
2025-10-17 19:47:10 +0200

    rahhh

--- battler/src/main.server.ts
+++ battler/src/main.server.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-// ✅ WICHTIG: context-Parameter vom Server übernehmen
+// ✅ Default-Export-Funktion, die vom Server aufgerufen wird
 export default function bootstrap(context: any) {
   return bootstrapApplication(AppComponent, appConfig, context);
 }
