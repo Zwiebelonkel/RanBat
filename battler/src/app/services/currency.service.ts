ed4ee62c088cdede24fc1187ab053ee26b5e15cb
lucamuller2004 <lucamuller2004@gmail.com>
2025-10-19 15:07:46 +0000

    fixxx

--- battler/src/app/services/currency.service.ts
+++ battler/src/app/services/currency.service.ts
@@ -1,7 +1,8 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { AuthService } from './auth.service';
-import { switchMap, of } from 'rxjs';
+import { switchMap, of, BehaviorSubject, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -12,16 +13,40 @@ export class CurrencyService {
   private authService = inject(AuthService);
   private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  private goldSubject = new BehaviorSubject<number>(0);
+  public gold$ = this.goldSubject.asObservable();
+
+  constructor() {
+    this.getGold().subscribe();
+  }
 
   getGold() {
-    return this.authService.currentUser.pipe(
+    return this.authService.currentUser$.pipe(
       switchMap(user => {
         if (!user) {
+          this.goldSubject.next(0);
           return of(null);
         }
-        return this.http.get<any>(`${this.apiUrl}/currency/gold/${user.id}`);
+        return this.http.get<any>(`${this.apiUrl}/currency/gold/${user.id}`).pipe(
+          tap(response => {
+            this.goldSubject.next(response.gold);
+          })
+        );
       })
     );
+  }
+
+  addGold(amount: number) {
+    const currentGold = this.goldSubject.getValue();
+    this.goldSubject.next(currentGold + amount);
+  }
+
+  spendGold(amount: number): boolean {
+    const currentGold = this.goldSubject.getValue();
+    if (currentGold >= amount) {
+      this.goldSubject.next(currentGold - amount);
+      return true;
+    }
+    return false;
   }
 }
